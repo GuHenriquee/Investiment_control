1fee35cc1f535ee618d31b9cd807219b39b4e3dd
GuHenriquee <gguu.henri@gmail.com>
2025-09-11 15:20:11 -0300

    error 422

--- src/app/componentes/dealer/dealer.ts
+++ src/app/componentes/dealer/dealer.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators, ɵInternalFormsSharedModule, ReactiveFormsModule } from '@angular/forms';
+import { OperationsService } from '../../services/operations-service';
+import { Operation } from './operation';
 
 @Component({
   selector: 'app-dealer',
-  imports: [],
+  imports: [ɵInternalFormsSharedModule, ReactiveFormsModule],
   templateUrl: './dealer.html',
   styleUrl: './dealer.css'
 })
-export class Dealer {
+
+export class Dealer implements OnInit {
+
+  operation!: FormGroup
+
+  constructor(private fb:FormBuilder, private op: OperationsService){}
+
+   ngOnInit() {
+    this.operation = this.fb.nonNullable.group({
+      amount: [0, Validators.min(1)],
+      operationType: [''],
+      previousValue:[0],
+    })
+}
+  
+  onSubmit(){
+    const data: Operation = this.operation.value
+    console.log("Enviando para a API:", data);
+
+    this.op.sendOperation(data).subscribe({ 
+        next: (newOperation) => {
+          console.log('Usuário criado com sucesso!', newOperation);
+        },
+        error: (err) => {
+          console.error('Ocorreu um erro:', err);
+        }
+      });
+  }
 
 }
--- src/app/services/operations-service.ts
+++ src/app/services/operations-service.ts
@@ -1,21 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpRequest } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Operation } from '../componentes/dealer/operation';
+import { ReceveOperation } from '../componentes/dealer/operation';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class OperationsService {
-  operationType(event: InputEvent):void{
-    if (String(event) == "retirar"){
-      
-      }
-    }
 
-  patrimonyAlter(type:string):void{
-    
+  private api = "http://127.0.0.1:8000/api/operation/"
+
+  constructor(private http: HttpClient){}
+
+  sendOperation(operation:Operation):Observable<ReceveOperation>{
+  
+    return this.http.post<ReceveOperation>(this.api,operation)
   }
-
-
-  }
+ 
+}
 
   
